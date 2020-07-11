8c427f7d50f5149d931d799748147df2e7a91942
OwenJAYEH <OwenJAYEH@gmail.com>
2020-07-11 15:26:52 -0700

    BOUNCY BOUNCE

---
+++ GMTK-Jam-2020/Assets/Scripts/Player Scripts/PlayerOneBouncy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneBouncy : MonoBehaviour
+{
+    public PhysicsMaterial2D slippery;
+    public PhysicsMaterial2D bouncy;
+
+    public bool bouncyBool = false;
+    public Collider2D boxCollider;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+    public void bouncyToggleOn()
+    {
+        bouncyBool = true;
+    }
+    public void bouncyToggleOff()
+    {
+        bouncyBool = false;
+    }
+
+    void Update()
+    {
+      if (bouncyBool == true)
+        {
+            boxCollider.sharedMaterial = bouncy;
+        }
+
+      if (bouncyBool == false)
+        {
+            boxCollider.sharedMaterial = slippery;
+        }
+    }
+}
