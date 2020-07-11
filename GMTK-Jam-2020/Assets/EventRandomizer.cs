292850edb585336006b9084bd34b9709643511c0
OwenJAYEH <OwenJAYEH@gmail.com>
2020-07-11 12:28:20 -0700

    Five RNG Modifiers Added

--- GMTK-Jam-2020/Assets/EventRandomizer.cs
+++ GMTK-Jam-2020/Assets/EventRandomizer.cs
@@ -9,35 +9,59 @@ public class EventRandomizer : MonoBehaviour
     public PlatformManager PlatformManagerScript;
     public SlowMotion SlowMotionP1Script;
     public SlowMotion SlowMotionP2Script;
+    public PlayerOneMovement PlayerOneScript;
+    public PlayerTwoMovement PlayerTwoScript;
+
+    public GameObject PlayerOne;
+    public GameObject PlayerTwo;
 
     public int randomizedInt;
 
     public void Randomizer()
     {
-        randomizedInt = Random.Range(0, 3);
+        randomizedInt = Random.Range(0, 5);
 
         PlatformManagerScript.dropBoolToggleOff();
         CameraFlipScript.flipCameraToggleOff();
         SlowMotionP1Script.slowMoToggleOff();
         SlowMotionP2Script.slowMoToggleOff();
+        PlayerOneScript.tripleSpeedToggleOff();
+        PlayerTwoScript.tripleSpeedToggleOff();
+        PlayerOne.GetComponent<PlayerOneMovement>().enabled = true;
+        PlayerTwo.GetComponent<PlayerTwoMovement>().enabled = true;
+        PlayerOne.GetComponent< PlayerOneMirrorMovement>().enabled = false;
+        PlayerTwo.GetComponent<PlayerTwoMirrorMovement>().enabled = false;
 
 
         switch (randomizedInt)
         {
             case 0:
-                CameraFlipScript.flipCameraToggleOn();
                 print("Camera Flip Event Triggered");
+                CameraFlipScript.flipCameraToggleOn();
                 break;
             case 1:
-                PlatformManagerScript.dropBoolToggleOn();
                 print("Drop Event Triggered");
+                PlatformManagerScript.dropBoolToggleOn();
                 break;
             case 2:
+                print("Slow Motion Event Triggered");
                 SlowMotionP1Script.slowMoToggleOn();
                 SlowMotionP2Script.slowMoToggleOn();
-                print("Slow Motion Event Triggered");
                 break;
-                
+            case 3:
+                print("Triple Speed Event Triggered");
+                PlayerOneScript.tripleSpeedToggleOn();
+                PlayerTwoScript.tripleSpeedToggleOn();
+                break;
+            case 4:
+                print("Mirrored Controls Triggered");
+                PlayerOne.GetComponent<PlayerOneMovement>().enabled = false;
+                PlayerTwo.GetComponent<PlayerTwoMovement>().enabled = false;
+                PlayerOne.GetComponent<PlayerOneMirrorMovement>().enabled = true;
+                PlayerTwo.GetComponent<PlayerTwoMirrorMovement>().enabled = true;
+                break;
+
+
 
         }
     }
