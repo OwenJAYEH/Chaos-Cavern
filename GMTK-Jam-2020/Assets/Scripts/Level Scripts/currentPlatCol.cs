bb5c07641bfa24592de5ae257d041ef90f6a7c93
Yarnuh <jaggerinyars@gmail.com>
2020-07-12 00:39:31 -0600

    miningANIMATIONworks

--- GMTK-Jam-2020/Assets/Scripts/Level Scripts/currentPlatCol.cs
+++ GMTK-Jam-2020/Assets/Scripts/Level Scripts/currentPlatCol.cs
@@ -10,6 +10,9 @@ public class currentPlatCol : MonoBehaviour
     public EventRandomizer RandomizerScript;
     public Timer TimerScript;
 
+    public Animator animator1;
+    public Animator animator2;
+
     bool playerOneGoal = false;
     bool playerTwoGoal = false;
 
@@ -19,26 +22,30 @@ public class currentPlatCol : MonoBehaviour
         if (col.gameObject.tag == "PlayerOne")
         {
             playerOneGoal = true;
+            animator1.SetBool("OnPurp1", true);
         }
 
         if (col.gameObject.tag == "PlayerTwo")
         {
             playerTwoGoal = true;
+            animator2.SetBool("OnPurp2", true);
         }
     }
 
 
     // Function makes booleans false when Player One or Player Two stops colliding with goal
-    void OnTriggerExit2D(Collider2D col)
+    public void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "PlayerOne")
         {
             playerOneGoal = false;
+            animator1.SetBool("OnPurp1", false);
         }
 
         if (col.gameObject.tag == "PlayerTwo")
         {
             playerTwoGoal = false;
+            animator2.SetBool("OnPurp2", false);
         }
     }
 
